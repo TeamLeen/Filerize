96b899778654afab8e15799d3d340f2a9a5a7905
Basel Akasha <contact@baselakasha.com>
2023-02-11 16:00:10 +0000

    User can select a folder

--- start_interface.py
+++ start_interface.py
@@ -1,11 +1,19 @@
 import eel
 import json
+import wx
 
 @eel.expose
 def get_config_file_content():
     with open("./config.json", "r") as config_file:
         config_json = json.loads(config_file.read())
     return config_json
+
+
+@eel.expose
+def choose_folder():
+    app = wx.App(None)
+    folder = wx.DirSelector(message="select")
+    return folder
 
 eel.init('web')
 eel.start('index.html', size=(1078, 800))
