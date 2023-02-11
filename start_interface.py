82ae144b33215fdebb6686fdafdf9ba8bf7541b6
Basel Akasha <contact@baselakasha.com>
2023-02-11 16:47:19 +0000

    WIP GUI

--- start_interface.py
+++ start_interface.py
@@ -8,7 +8,21 @@ def get_config_file_content():
         config_json = json.loads(config_file.read())
     return config_json
 
+@eel.expose
+def add_new_folder(folder_path, folder_summary):
+    with open("./config.json", "r") as config_file:
+        config_json = json.loads(config_file.read())
 
+    config_json["folders"].append({
+        "path": folder_path,
+        "summary": folder_summary
+    })
+
+    with open("./config.json", "w") as config_file:
+        config_file.write(json.dumps(config_json))
+
+    return True
+    
 @eel.expose
 def choose_folder():
     app = wx.App(None)
