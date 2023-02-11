d862d2467fd0c25c510c3c4953bae1f1de6c0a73
Jian <86570135+sanamorii@users.noreply.github.com>
2023-02-11 15:09:21 +0000

    moved parsing to seperate folder

--- main.py
+++ main.py
@@ -18,22 +18,6 @@ labels = {
     # 'comp1008': 'Artifitial Intelligence'
 }
 
-
-parser = argparse.ArgumentParser(prog="Filerize", 
-                                 description="Document sorter using GPT-3",
-                                 epilog="TEAMLEAN @ NO COPYRIGHT 2023 :^)")
-parser.add_argument("directory", type=str)
-args = parser.parse_args()
-
-def parsing():
-    folder = fstructs.Folder(path = args.directory)
-    folder, count = ftools.crawl(folder=folder)
-
-    # for dir in folder.subfolders:
-    #     print(dir.path)
-
-# test
-
 def main():
     ## debug
 
