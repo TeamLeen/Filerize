16e22096d8dde2f98a3e6247c842ae679e725f34
Jian <86570135+sanamorii@users.noreply.github.com>
2023-02-12 00:45:02 +0000

    forgor the git add again 💀

--- api.py
+++ api.py
@@ -29,8 +29,6 @@ async def label_folder(folder: fstructs.Folder):
 
 
 async def init(args):
-    Config.load()
-    Config.set_src_folder('test_files')
 
     folder = fstructs.Folder(path=args.directory)
     await ftools.recursive_visit(folder=folder, visit=label_folder)
--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import argparse
 import asyncio
+import logging
+import os, sys
 
+from config import Config
 import api
 
 parser = argparse.ArgumentParser(prog="Filerize",
@@ -9,3 +12,33 @@ parser = argparse.ArgumentParser(prog="Filerize",
 parser.add_argument("directory", type=str)
 parser.add_argument("-v", "--verbose", action="store_true")
 args = parser.parse_args()
+
+
+
+def main():
+    if args.verbose: logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+    
+    if os.path.exists('config.json'):
+        Config.load('config.json')
+    elif os.path.exists('config.example.json'):  # fallback
+        Config.load('config.example.json')
+    else:
+        print("No config file detected...\nInitialising & creating config file.")
+        
+        while True:
+            pth = sum = None
+            pth = str(input("Enter folder path: "))
+            sum = str(input("Enter folder summary: "))
+            
+            if pth == "q" or sum == "q":
+                break
+        
+            # Config.add_label(label=pth, summary=sum)
+            
+            
+        
+    
+
+if __name__ == "__main__":
+    main()
+
