e38423242c15779e92b082f47c5d5a7138514f7b
Jian <86570135+sanamorii@users.noreply.github.com>
2023-02-12 11:15:38 +0000

    finished cli interface

--- cli.py
+++ cli.py
@@ -3,6 +3,7 @@ import logging
 import os, sys
 
 import click
+import threading
 
 from config.config import Config
 from config import globalvar as gv
@@ -17,7 +18,8 @@ _common_options = [
 
 _config_options = [
     click.option("--add", type = str, default = None),
-    click.option("--delete", type = str, default = None)
+    click.option("--delete", type = str, default = None),
+    click.option("--show", default = None, count=True)
 ]
 
 
@@ -43,13 +45,11 @@ def cli(verbose):
 @click.help_option("-h", "--help")
 @click.pass_context
 def cli_start(ctx:click.Context, **args):
+    check_directory(directory=args['directory'])
     folder = Filerize.init(args['directory'])
     Filerize.sort(folder=folder)
-    
-    
-    # check_directory(args['directory'])
-    # check_config()
-    # asyncio.run(Filerize.sort(path=args['directory']))
+
+    Filerize.listen(folder=folder)
     # run continous behaviour
     
 
@@ -59,6 +59,7 @@ def cli_start(ctx:click.Context, **args):
 @click.help_option("-h", "--help")
 @click.pass_context
 def cli_sort(ctx:click.Context, **args):
+    check_directory(directory=args['directory'])
     folder = Filerize.init(args['directory'])
     Filerize.sort(folder=folder)
     
@@ -67,6 +68,7 @@ def cli_sort(ctx:click.Context, **args):
 @click.help_option("-h", "--help")
 @click.pass_context
 def cli_listen(ctx:click.Context, **args):
+    check_directory(directory=args['directory'])
     folder = Filerize.init(args['directory'])
     Filerize.listen(folder=folder)
 
@@ -76,33 +78,64 @@ def cli_listen(ctx:click.Context, **args):
 @click.help_option("-h", "--help")
 @click.pass_context
 def cli_config(ctx:click.Context, **args):
-    print("NOT IMPLEMENTED")
-    pass
 
-@cli.command("debug", help="debug - not to be in production")
-@click.help_option("-h", "--help")
-@click.pass_context
-def cli_debug(ctx:click.Context, **args):
-    Config.load(cfg_path=gv.DEFAULT_CONFIG_PATH)
-    label = asyncio.run(Filerize.label_file(r"D:\dev\hacknotts\23\Filerize\testing\untouched\COMP1004-MySQL-setup.docx"))
-    print(label)
-    pass
 
-@cli.command("recursive_print", help="debug - not to be in production")
-@click.help_option("-h", "--help")
-@click.pass_context
-def cli_debug(ctx:click.Context, **args):
-    asyncio.run(Filerize.sort(path=args['directory']))
-    pass
+    if all([args["add"], args["delete"], args["show"]]) or not all([args["add"], args["delete"], args["show"]]):
+        print("usage: --add | --delete | --show | (not all)")
+        return
+    else:
+        Config.load(gv.DEFAULT_CONFIG_PATH)
+        
+    if args['add']:
+        
+        pth = os.path.abspath(args["add"])
+        
+        if os.path.exists(path=pth):
+            sum = str(input(f"Enter directory summary: "))
+            if sum == "":
+                print("Error: no summary. Aborting...")
+                return
+            Config.add_label(label=pth, summary=sum)
+            Config.save()
+        else:
+            print("Error: directory does not exist")
+            return
+    elif args["delete"]:
+        pth = os.path.abspath(args["delete"])
+        stat = Config.delete_label(label=pth)
+        if stat:
+            print("Entry Deleted")
+        else:
+            print("Error: entry does not exist")
+        
+        Config.save()
+            
+    elif args["show"]:
+        Config.print_config()
+        
+        
+
+# @cli.command("debug", help="debug - not to be in production")
+# @click.help_option("-h", "--help")
+# @click.pass_context
+# def cli_debug(ctx:click.Context, **args):
+#     Config.load(cfg_path=gv.DEFAULT_CONFIG_PATH)
+#     label = asyncio.run(Filerize.label_file(r"D:\dev\hacknotts\23\Filerize\testing\untouched\COMP1004-MySQL-setup.docx"))
+#     print(label)
+#     pass
+
+# @cli.command("recursive_print", help="debug - not to be in production")
+# @click.help_option("-h", "--help")
+# @click.pass_context
+# def cli_debug(ctx:click.Context, **args):
+#     asyncio.run(Filerize.sort(path=args['directory']))
+#     pass
 
     
 def check_directory(directory:str = None) -> None:
     if not os.path.exists(directory):
         print("Invalid directory given. Aborting...")
         exit()
-    
-    
-
 
 
 if __name__ == "__main__":
